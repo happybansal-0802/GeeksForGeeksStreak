8320a51dd1f54f858cf47e470fb2e79db74902e2
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-06-20 10:21:45 +0530

    Create Matchsticks game.java

---
+++ Matchsticks game.java
@@ -0,0 +1,38 @@
+Two friends, A and B, are playing the game of matchsticks. In this game, a group of N matchsticks is placed on the table. The players can pick any number of matchsticks from 1 to 4 (both inclusive) during their chance. The player who takes the last match stick wins the game. If A starts first, how many matchsticks should he pick on his 1st turn such that he is guaranteed to win the game or determine if it's impossible for him to win. Return -1 if it's impossible for A to win the game, else return the number of matchsticks should he pick on his 1st turn such that he is guaranteed to win.
+Note : Consider both A and B play the game optimally.
+  Solution:
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.matchGame(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int matchGame(Long N) {
+           if(N%5==0){
+            return -1;
+            
+        }
+       
+        int rem=(int)(N%5);
+        return rem;
+    }
+};
