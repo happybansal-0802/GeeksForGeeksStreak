643d7a382b128816fc8f9500cb5c32969e1bd373
Happy Bansal <happy.bansal_cs20@gla.ac.in>
2023-07-18 21:49:03 +0530

    Create Longest Repeating Subsequence.java

---
+++ Longest Repeating Subsequence.java
@@ -0,0 +1,53 @@
+Given string str, find the length of the longest repeating subsequence such that it can be found twice in the given string.
+
+The two identified subsequences A and B can use the same ith character from string str if and only if that ith character has different indices in A and B. For example, A = "xax" and B = "xax" then the index of first "x" must be different in the original string for A and B.
+  Solution:
+  //{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+         char arr[]=str.toCharArray();
+        int max=0;
+        int n=arr.length;
+        int[][]dp=new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                if(arr[i-1]==arr[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+                 max=Math.max(max,dp[i][j]);
+            }
+        }
+        return max;
+    }
+}
